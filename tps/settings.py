05aae28c3333c1a23252c387a5bccf914bbb053f
Kaya Oguz <kayaoguz@gmail.com>
2026-02-14 17:46:04 +0300

    Load environment variables from .env file for improved configuration management

--- tps/settings.py
+++ tps/settings.py
@@ -13,8 +13,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
