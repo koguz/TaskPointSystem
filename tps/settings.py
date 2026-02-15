ea0a14a2e76c719b539c85f36becbe7c9208da3f
Kaya Oguz <kayaoguz@gmail.com>
2026-02-15 23:37:36 +0300

    Update SECRET_KEY in settings to use environment variable for improved security

--- tps/settings.py
+++ tps/settings.py
@@ -25,7 +25,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z-t^6m!qh8=lbw6la(sdvy!#wmeausu_5w&h-x1rq(6u8yup3q'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-only-key-change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
